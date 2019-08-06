bcf1bed0161fb9c937518bd43a9991b1044d303b
Brett Parsekian <bparsekian@gmail.com>
2019-08-06 14:36:19 -0700

    Moving model outside of json unit tests. Adding final assertions to test.

---
+++ Geofencer/Geofencer/SavedPlaces.swift
@@ -0,0 +1,96 @@
+//
+//  File.swift
+//  Geofencer
+//
+//  Created by Brett Parsekian on 8/6/19.
+//  Copyright © 2019 bparsekian. All rights reserved.
+//
+
+import Foundation
+
+struct SavedPlaces {
+    var places: [Place]
+}
+
+struct Place: Decodable {
+    let title: String
+    let coordinates: GeoCoordinates
+}
+
+struct GeoCoordinates {
+    var latitude : Double
+    var longitude: Double
+}
+
+extension GeoCoordinates : Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case latitude = "Latitude"
+        case longitude = "Longitude"
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let latitudeString = try container.decode(String.self, forKey: .latitude)
+        let longitudeString = try container.decode(String.self, forKey: .longitude)
+        latitude = Double(latitudeString) ?? 0
+        longitude = Double(longitudeString) ?? 0
+    }
+}
+
+struct Location : Decodable {
+    let geoCoordinates : GeoCoordinates
+    
+    private enum CodingKeys: String, CodingKey {
+        case geoCoordinates = "Geo Coordinates"
+        case latitude = "Latitude"
+        case longitude = "Longitude"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let gCoordinates = try container.decodeIfPresent(GeoCoordinates.self, forKey: .geoCoordinates)
+        if (gCoordinates == nil) {
+            let latitudeString = try container.decode(String.self, forKey: .latitude)
+            let longitudeString = try container.decode(String.self, forKey: .longitude)
+            geoCoordinates = GeoCoordinates(latitude : Double(latitudeString) ?? 0,
+                                            longitude: Double(longitudeString) ?? 0)
+        }
+        else {
+            geoCoordinates = gCoordinates ?? GeoCoordinates(latitude : 0, longitude: 0)
+        }
+    }
+}
+
+struct Properties : Decodable {
+    let location: Location
+    let title : String
+    
+    private enum CodingKeys: String, CodingKey {
+        case location = "Location"
+        case title = "Title"
+    }
+}
+
+struct Feature : Decodable {
+    let properties : Properties
+    
+    private enum CodingKeys: String, CodingKey {
+        case properties = "properties"
+    }
+}
+
+extension SavedPlaces: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case type = "type"
+        case features = "features"
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let features = try container.decode([Feature].self, forKey: .features)
+        places = []
+        for feature in features {
+            let newPlace = Place(title: feature.properties.title,
+                                 coordinates: feature.properties.location.geoCoordinates)
+            places.append(newPlace)
+        }
+    }
+}
--- Geofencer/GeofencerTests/GeofencerTests.swift
+++ Geofencer/GeofencerTests/GeofencerTests.swift
@@ -9,105 +9,7 @@
 import XCTest
 @testable import Geofencer
 
-struct Place: Decodable {
-    let title: String
-    let coordinates: GeoCoordinates
-}
 
-struct GeoCoordinates {
-    var latitude : Double
-    var longitude: Double
-}
-
-extension GeoCoordinates : Decodable {
-    enum CodingKeys: String, CodingKey {
-        case latitude = "Latitude"
-        case longitude = "Longitude"
-    }
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let latitudeString = try container.decode(String.self, forKey: .latitude)
-        let longitudeString = try container.decode(String.self, forKey: .longitude)
-        latitude = Double(latitudeString) ?? 0
-        longitude = Double(longitudeString) ?? 0
-    }
-}
-
-struct Location : Decodable {
-    let geoCoordinates : GeoCoordinates
-    
-    enum CodingKeys: String, CodingKey {
-        case geoCoordinates = "Geo Coordinates"
-        case latitude = "Latitude"
-        case longitude = "Longitude"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let gCoordinates = try container.decodeIfPresent(GeoCoordinates.self, forKey: .geoCoordinates)
-        if (gCoordinates == nil) {
-            let latitudeString = try container.decode(String.self, forKey: .latitude)
-            let longitudeString = try container.decode(String.self, forKey: .longitude)
-            geoCoordinates = GeoCoordinates(latitude : Double(latitudeString) ?? 0,
-                                            longitude: Double(longitudeString) ?? 0)
-        }
-        else {
-            geoCoordinates = gCoordinates!
-        }
-    }
-}
-
-struct Properties : Decodable {
-    let location: Location
-    let title : String
-    
-    enum CodingKeys: String, CodingKey {
-        case location = "Location"
-        case title = "Title"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        location = try container.decode(Location.self, forKey: .location)
-        title = try container.decode(String.self, forKey: .title)
-    }
-}
-
-struct Feature : Decodable {
-    let properties : Properties
-    
-    enum CodingKeys: String, CodingKey {
-        case properties = "properties"
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        properties = try container.decode(Properties.self, forKey: .properties)
-    }
-}
-
-struct SavedPlaces {
-    var features: [Feature]
-    var places: [Place]
-}
-
-extension SavedPlaces: Decodable {
-    enum CodingKeys: String, CodingKey {
-        case type = "type"
-        case features = "features"
-    }
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        features = try container.decode([Feature].self, forKey: .features)
-        places = []
-        for feature in features {
-            //print(feature)
-            let newPlace = Place(title: feature.properties.title,
-                                 coordinates: feature.properties.location.geoCoordinates)
-            places.append(newPlace)
-        }
-    }
-}
 
 
 
@@ -133,18 +35,66 @@ class GeofencerTests: XCTestCase {
         }
     }
     
-    
-    
     func testJsonDecoder() {
         guard let url = Bundle.main.url(forResource: "Saved Places", withExtension: "json") else { return }
         guard let json = try? Data(contentsOf: url) else { return }
         let decoder = JSONDecoder()
-        let geoPointAccuracy = 0.00001
+        let geoPointAccuracy = 0.0000001
         do {
             let savedPlaces = try decoder.decode(SavedPlaces.self, from: json)
             XCTAssertEqual(savedPlaces.places[0].coordinates.latitude, 34.1413891, accuracy: geoPointAccuracy)
             XCTAssertEqual(savedPlaces.places[0].coordinates.longitude, -118.1507140, accuracy: geoPointAccuracy)
             XCTAssertEqual(savedPlaces.places[0].title, "Select Physical Therapy")
+            
+            XCTAssertEqual(savedPlaces.places[1].coordinates.latitude, 34.1471415, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[1].coordinates.longitude, -118.2464751, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[1].title, "Dollar Tree")
+            
+            XCTAssertEqual(savedPlaces.places[2].coordinates.latitude, 34.1537760, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[2].coordinates.longitude, -118.2428634, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[2].title, "Virgil's Hardware Home Center")
+            
+            XCTAssertEqual(savedPlaces.places[3].coordinates.latitude, 34.115110, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[3].coordinates.longitude, -118.248561, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[3].title, "Goodwill")
+            
+            XCTAssertEqual(savedPlaces.places[4].coordinates.latitude, 34.1364105, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[4].coordinates.longitude, -118.2589307, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[4].title, "Salvation Army Corps & Community Center")
+            
+            XCTAssertEqual(savedPlaces.places[5].coordinates.latitude, 34.1486856, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[5].coordinates.longitude, -118.2346624, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[5].title, "Smart & Final Extra!")
+            
+            XCTAssertEqual(savedPlaces.places[6].coordinates.latitude, 34.1429980, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[6].coordinates.longitude, -118.2586030, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[6].title, "Target")
+            
+            XCTAssertEqual(savedPlaces.places[7].coordinates.latitude, 48.8861929, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[7].coordinates.longitude, 2.3430895, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[7].title, "Montmartre")
+            
+            XCTAssertEqual(savedPlaces.places[8].coordinates.latitude, 48.8462217, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[8].coordinates.longitude, 2.3371605, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[8].title, "Luxembourg Gardens")
+            
+            XCTAssertEqual(savedPlaces.places[9].coordinates.latitude, 48.8583424, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[9].coordinates.longitude, 2.3375084, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[9].title, "Pont des Arts")
+            
+            XCTAssertEqual(savedPlaces.places[10].coordinates.latitude, 34.0773927, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[10].coordinates.longitude, -118.2208076, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[10].title, "Society of St. Vincent de Paul Los Angeles Thrift Store")
+            
+            XCTAssertEqual(savedPlaces.places[11].coordinates.latitude, 34.0946477, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[11].coordinates.longitude, -118.1284956, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[11].title, "In Motion Sports")
+            
+            XCTAssertEqual(savedPlaces.places[12].coordinates.latitude, 34.1492278, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[12].coordinates.longitude, -118.0739699, accuracy: geoPointAccuracy)
+            XCTAssertEqual(savedPlaces.places[12].title, "Harbor Freight Tools")
+            
+            
             //all fine with jsonData here
         } catch {
             //handle error
