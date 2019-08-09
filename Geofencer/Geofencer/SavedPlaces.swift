fa959b59253ea7f45fb0204d0c6d09466570e584
Brett Parsekian <bparsekian@gmail.com>
2019-08-09 10:01:30 -0700

    Adding code review suggestions to SavedPlaces

--- Geofencer/Geofencer/SavedPlaces.swift
+++ Geofencer/Geofencer/SavedPlaces.swift
@@ -12,6 +12,23 @@ struct SavedPlaces {
     var places: [Place]
 }
 
+extension SavedPlaces: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case type = "type"
+        case features = "features"
+    }
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let features = try container.decode([Feature].self, forKey: .features)
+        places = []
+        for feature in features {
+            let newPlace = Place(title: feature.properties.title,
+                                 coordinates: feature.properties.location.geoCoordinates)
+            places.append(newPlace)
+        }
+    }
+}
+
 struct Place: Decodable {
     let title: String
     let coordinates: GeoCoordinates
@@ -48,7 +65,7 @@ struct Location : Decodable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         let gCoordinates = try container.decodeIfPresent(GeoCoordinates.self, forKey: .geoCoordinates)
-        if (gCoordinates == nil) {
+        if gCoordinates == nil {
             let latitudeString = try container.decode(String.self, forKey: .latitude)
             let longitudeString = try container.decode(String.self, forKey: .longitude)
             geoCoordinates = GeoCoordinates(latitude : Double(latitudeString) ?? 0,
@@ -75,22 +92,5 @@ struct Feature : Decodable {
     
     private enum CodingKeys: String, CodingKey {
         case properties = "properties"
-    }
-}
-
-extension SavedPlaces: Decodable {
-    private enum CodingKeys: String, CodingKey {
-        case type = "type"
-        case features = "features"
-    }
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let features = try container.decode([Feature].self, forKey: .features)
-        places = []
-        for feature in features {
-            let newPlace = Place(title: feature.properties.title,
-                                 coordinates: feature.properties.location.geoCoordinates)
-            places.append(newPlace)
-        }
     }
 }
